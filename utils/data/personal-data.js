f394b4ab5b3f11ba4d476f73553fa386db3d77ed
gg2299 <gg2299@gmail.com>
2025-04-29 22:08:02 -0400

    update name

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,8 +1,8 @@
 export const personalData = {
-  name: "Novak Isakov",
+  name: "Garrett Grimes",
   profile: '/portfolio-website/profile2.png',
   designation: "Software Developer",
-  description: "My name is Novak Isakov. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "My name is Garrett Grimes. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'alexkcoding@gmail.com',
   phone: '+381 65 8025383',
   address: 'Dalmatinska 7, Sombor, Serbia - 25000',
