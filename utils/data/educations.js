3e50e80f0acbd8366c138902f642a1a742281c3d
gg2299 <gg2299@gmail.com>
2025-04-29 22:28:41 -0400

    update education

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor of Science in Computer Science",
-    duration: "2017 - 2021",
-    institution: "University of Belgrade",
+    duration: "2016 - 2020",
+    institution: "University of California, Los Angeles (UCLA)",
   },
   {
     id: 2,
     title: "High School Diploma",
-    duration: "2013 - 2017",
-    institution: "Mathematical Grammar School, Belgrade",
+    duration: "2012 - 2016",
+    institution: "Theodore Roosevelt High School, Los Angeles, CA",
   },
   {
     id: 3,
     title: "Middle School",
-    duration: "2007 - 2013",
-    institution: "Elementary School Vladislav Ribnikar, Belgrade",
+    duration: "2009 - 2012",
+    institution: "Hollenbeck Middle School, Los Angeles, CA",
   }
 ];
